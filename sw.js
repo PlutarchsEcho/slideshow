c52d714f3be8a9e8a4ac0549b626aeb4e2986f86
plutarchsecho <plutarchsecho@proton.me>
2026-03-24 15:36:16 -0700

    Fix: bump SW cache to v3, include styles.css, force update on activate

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const CACHE = 'slideshow-v1';
+const CACHE = 'slideshow-v3';
 const ASSETS = [
   './slideshow.html',
-  './manifest.json'
+  './styles.css',
+  './manifest.json',
+  './display.html'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
